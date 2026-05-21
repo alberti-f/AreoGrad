86a87abeffa999a630247db18886dd8bd64bf631
alberti-f <fnc.alberti@gmail.com>
2026-05-21 13:59:00 +0100

    use proper os syntax

--- scripts/07.stack_data.py
+++ scripts/07.stack_data.py
@@ -14,19 +14,16 @@ gradient_paths = SMK.input["gradient_paths"]
 dataset = vu.dataset(dataset_id)
 
 area_all = np.array([np.load(p) for p in area_paths])
-if not os.path.exists(os.path.dirname(SMK.output["area_out"])):
-    os.makedirs(os.path.dirname(SMK.output["area_out"]))
+os.makedirs(os.path.dirname(SMK.output["area_out"]), exist_ok=True)
 np.save(SMK.output["area_out"], area_all)
 
 gradients_all = np.array([np.load(p) for p in gradient_paths])
-if not os.path.exists(os.path.dirname(SMK.output["gradient_out"])):
-    os.makedirs(os.path.dirname(SMK.output["gradient_out"]))
+os.makedirs(os.path.dirname(SMK.output["gradient_out"]), exist_ok=True)
 np.save(SMK.output["gradient_out"], gradients_all)
 
 dispersion_all = np.array([
     [np.mean(pairwise_distances(g[:, None], metric="euclidean")) for g in subj.T]
     for subj in gradients_all
     ])
-if not os.path.exists(os.path.dirname(SMK.output["dispersion_out"])):
-    os.makedirs(os.path.dirname(SMK.output["dispersion_out"]))
+os.makedirs(os.path.dirname(SMK.output["dispersion_out"]), exist_ok=True)
 np.save(SMK.output["dispersion_out"], dispersion_all)
