3aab831679035fb9ac9724a18539f266fc9b5c74
alberti-f <fnc.alberti@gmail.com>
2026-05-21 13:53:58 +0100

    use parcellation file instead of brainspaceparcellation

--- scripts/02.parcel_area.py
+++ scripts/02.parcel_area.py
@@ -12,14 +12,23 @@ subj_id = SMK.wildcards["subj_id"]
 dataset_id = SMK.config["dataset_id"]
 parcellation = SMK.config["parcellation"]
 parcellation_scale = SMK.config["parcellation_scale"]
-
+parcellation_path = SMK.params["parcellation_path"]
 area_dscalar = SMK.input[0]
+
 
 idx_cortex = np.hstack([hcp.vertex_info.grayl, hcp.vertex_info.grayr + hcp.vertex_info.num_meshl])
 
 subj = vu.subject(subj_id, dataset_id)
 area = nib.load(area_dscalar).get_fdata().squeeze()[idx_cortex]
-labels = load_parcellation(parcellation, scale=parcellation_scale, join=True)[idx_cortex]
+# labels = load_parcellation(parcellation, scale=parcellation_scale, join=True)[idx_cortex]
+labels = nib.load(parcellation_path).get_fdata().squeeze().astype(int)
+if labels.size==64984:
+    labels = labels[idx_cortex]
+elif labels.size==59412:
+    labels = labels
+else:
+    raise ValueError("Unexpected number of vertices in parcellation labels")
+
 labs_start = 1 if 0 in labels else 0
 area_parcs = reduce_by_labels(area, labels, red_op=np.sum)[labs_start:]
 
