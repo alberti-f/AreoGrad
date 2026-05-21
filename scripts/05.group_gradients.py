91a0ce5fa68d8294f741ec850409366c97fa16b4
alberti-f <fnc.alberti@gmail.com>
2026-05-21 13:56:05 +0100

    use parcellation file instead of brainspaceparcellation and parametrize hemisphere splitting

--- scripts/05.group_gradients.py
+++ scripts/05.group_gradients.py
@@ -4,6 +4,7 @@ from brainspace.gradient import GradientMaps
 import numpy as np
 import hcp_utils as hcp
 import variograd_utils as vu
+import nibabel as nib
 from brainspace.datasets import load_parcellation
 SMK = snakemake
 
@@ -16,25 +17,41 @@ threshold = SMK.params["threshold"]
 approach = SMK.params["approach"]
 kernel = SMK.params["kernel"]
 random_state = SMK.params["random_state"]
-
-labels_l, labels_r = load_parcellation(parcellation, scale=parcellation_scale, join=False)
-labels_l, labels_r = np.unique(labels_l[hcp.vertex_info.grayl])[1:], np.unique(labels_r[hcp.vertex_info.grayr])[1:]
-n_l, n_r = labels_l.size, labels_r.size
+parcellation_path = SMK.params["parcellation_path"]
+split_hemispheres = SMK.params["split_hemispheres"]
 
 W = np.load(SMK.input[0])
 
-W_l = W[:n_l, :n_l].copy()
-gm_l = GradientMaps(n_components=n_components, approach=approach, 
-                    kernel=kernel, random_state=random_state)
-gm_l.fit(W_l, sparsity=threshold)
-gradients_l = gm_l.gradients_ 
+if split_hemispheres:
+    vinfo = hcp.vertex_info
+    labels = nib.load(parcellation_path).get_fdata().squeeze()
+    cortex_l = vinfo.grayl if labels.size==64984 else np.arange(vinfo.grayl.size)
+    cortex_r = vinfo.grayr + vinfo.num_meshl if labels.size==64984 else np.arange(vinfo.grayr.size) + cortex_l.size
+    labels_l, labels_r = labels[cortex_l], labels[cortex_r]
+    start_l = 1 if 0 in labels_l else 0
+    start_r = 1 if 0 in labels_r else 0
+    labels_l, labels_r = np.unique(labels_l)[start_l:], np.unique(labels_r)[start_r:]
+    n_l, n_r = labels_l.size, labels_r.size
 
-W_r = W[n_l: , n_l: ].copy()
-gm_r = GradientMaps(n_components=n_components, approach=approach,
-                    kernel=kernel,random_state=random_state)
-gm_r.fit(W_r, sparsity=threshold)
-gradients_r = gm_r.gradients_
 
-gradients = np.vstack([gradients_l, gradients_r])
+    W_l = W[:n_l, :n_l].copy()
+    gm_l = GradientMaps(n_components=n_components, approach=approach, 
+                        kernel=kernel, random_state=random_state)
+    gm_l.fit(W_l, sparsity=threshold)
+    gradients_l = gm_l.gradients_ 
+
+    W_r = W[n_l: , n_l: ].copy()
+    gm_r = GradientMaps(n_components=n_components, approach=approach,
+                        kernel=kernel,random_state=random_state)
+    gm_r.fit(W_r, sparsity=threshold)
+    gradients_r = gm_r.gradients_
+
+    gradients = np.vstack([gradients_l, gradients_r])
+
+else:
+    gm = GradientMaps(n_components=n_components, approach=approach, kernel=kernel, random_state=random_state)
+    gm.fit(W, sparsity=threshold)
+    gradients = gm.gradients_
+
 dataset = vu.dataset(dataset_id)
 np.save(SMK.output[0], gradients)
